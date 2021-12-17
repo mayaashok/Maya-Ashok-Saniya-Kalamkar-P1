e82f95e65d77c983a118aff47ab826412c64b4f9
mayaashok <67753867+mayaashok@users.noreply.github.com>
2021-12-17 11:08:21 -0800

    Created index.php with navigation bar

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="topnavbar.css"/>
+    <div class="topnav">
+        <a class='active' href='index.php'>Home</a>";
+        <a href='entertask-ui.php'>Enter-Tasks</a>";
+        <a href='viewtasks-ui.php'>View-Tasks</a>";
+    </div>
+  </head>
+ 
+  <body>
+ 
+  <header>
+    <div class="center">
+      <h1>Welcome to Planner 101!</h1>
+    </div>
+  </header>
+ 
+  </body>
+</html>
+ 
+<style>
+  .center {
+    margin: auto;
+    width: 50%;
+    padding: 10px;
+  }
+</style>
