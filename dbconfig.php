6947bc5c6827740751a127cfae020298e897927f
mayaashok <mayaashok47@gmail.com>
2021-12-18 14:09:50 -0800

    Created database connection file

---
+++ dbconfig.php
@@ -0,0 +1,9 @@
+<?php
+// Database connection settings
+$serverName = "localhost";
+$dBUsername = "dbuser1";
+$dBPassword = "!dbuser1!";
+$dBName     = "plannerDB";
+$port       = 3306;
+
+?>
--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="topnavbar.css"/>
     <div class="topnav">
-        <a class='active' href='index.php'>Home</a>";
-        <a href='entertask-ui.php'>Enter Tasks</a>";
-        <a href='viewtasks-ui.php'>View Tasks</a>";
+        <a class='active' href='index.php'>Home</a>
+        <a href='entertask-ui.php'>Enter Tasks</a>
+        <a href='viewtasks-ui.php'>View Tasks</a>
   </div> 
 
   </head>
