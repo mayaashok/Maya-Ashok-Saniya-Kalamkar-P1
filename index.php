1cbe0017e1949fdc0222336eb11e4050152aa2c2
SaniyaKalamkar <68800346+SaniyaKalamkar@users.noreply.github.com>
2021-12-17 17:19:56 -0800

    added diary.jpeg, added background and text, but needed too many <br>

--- index.php
+++ index.php
@@ -5,18 +5,28 @@
     <link rel="stylesheet" type="text/css" href="topnavbar.css"/>
     <div class="topnav">
         <a class='active' href='index.php'>Home</a>";
-        <a href='entertask-ui.php'>Enter-Tasks</a>";
-        <a href='viewtasks-ui.php'>View-Tasks</a>";
-    </div>
+        <a href='entertask-ui.php'>Enter Tasks</a>";
+        <a href='viewtasks-ui.php'>View Tasks</a>";
+  </div> 
+
   </head>
  
-  <body>
+  <body class="bgimg-1">
+  
+  <br> </br><br> </br><br> </br><br> </br><br> </br><br> </br> <br> </br> <br> </br> <br> </br> <br> </br> <br> </br> <br> </br> <br> </br> <br> </br> <br> </br>
+  <br> </br> <br> </br> <br> </br> <br> </br> <br> </br> <br> </br>
  
   <header>
-    <div class="center">
-      <h1>Welcome to Planner 101!</h1>
+      <div class="top">
+        <span style="font-size: 36px">Start something that really matters</span
+        ><br />
+        <br> </br>
+        <span style="font-size: 18px"
+          >Stop wasting valuable time with projects-that just isn't you.
+        </span>
     </div>
-  </header>
+</header>
+
  
   </body>
 </html>
@@ -26,5 +36,27 @@
     margin: auto;
     width: 50%;
     padding: 10px;
+  }
+  body {
+    font-family: "Raleway", sans-serif;
+  }
+  header {
+    position: relative;
+    filter: grayscale(50%);
+  }
+  .bgimg-1 {
+    background-position: center;
+    background-size: cover;
+    background-image: url("diary.jpeg");
+    min-height: 100%;
+  }
+  .top {
+    padding: 48px;
+    position: absolute;
+    top: 50%;
+    left: 0%;
+    transform: translate(0%, -50%);
+    -ms-transform: translate(-0%, -50%);
+    color: #fff;
   }
 </style>
